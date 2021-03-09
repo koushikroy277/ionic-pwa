b6a4092ffda3a6a9af5e207507c164390a44d43c
Koushik Roy <roykoushik275@gmail.com>
2021-03-09 17:37:04 +0600

    init

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
-const Tab1: React.FC = () => {
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab1.scss";
+
+import axios from "axios";
+
+const options = {
+  method: 'GET',
+  url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/extract',
+  params: {url: 'http://www.melskitchencafe.com/the-best-fudgy-brownies/'},
+  headers: {
+    'x-rapidapi-key': '95f1fb94d7mshe7816815b93cc4ap197e2fjsn4b824aff24ce',
+    'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+  }
+};
+
+export default function Tab1() {
+  axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   return (
-    <IonPage>
+    <IonPage className="ionic-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle className="ionic-head">Ecommerce</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div><h1>API CALL</h1></div>
       </IonContent>
     </IonPage>
   );
-};
-
-export default Tab1;
+}
